01ee9168696e2d14302c34233be143079cf74191
Codefire <64688963+codefire9@users.noreply.github.com>
2021-06-11 13:13:25 +0530

    us

--- Sql/SELECT/2. Event Hall-Number of booking customer wise/sample.sql
+++ Sql/SELECT/2. Event Hall-Number of booking customer wise/sample.sql
@@ -6,3 +6,17 @@ where h.event_date like '2020%'
 group by c.customer_id
 having c.customer_name like 'S%'
 order by 2;
+
+
+
+<or>
+
+//priority
+select  distinct c.customer_id as CUSTOMER_ID,c.customer_name as CUSTOMER_NAME,count(h.hall_id) as NO_OF_BOOKING
+from t_customer_details c
+right join t_hall_booking h
+on c.customer_id = h.customer_id
+where h.event_date like '2020%'
+group by c.customer_id
+having c.customer_name like 'S%'
+order by customer_ name;
